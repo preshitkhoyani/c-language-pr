16065bb7f85a0f9c1e6a2e2a3b220218278f2873
preshitkhoyani <hinagirish0306@gmail.com>
2025-05-17 15:26:13 +0530

    Update pr8_1.c

--- pr8_1.c
+++ pr8_1.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
-
-
-void har(int *ptr, int s)
-{
-    for (int i = 0; i < s * s; i++) 
-	{
-        printf("%d\t", (*(ptr + i)) * (*(ptr + i)) * (*(ptr + i)));
-    }
-    printf("\n");
-}
+#include<string.h>
 
 int main() 
+
 {
-    int s;
-    printf("Enter array's s: ");
-    scanf("%d", &s);
+    char a[100];
+    char *p;
+    int len;
 
-    int a[s][s];
+    printf("Enter any string: ");
+    gets(a); 
 
+    p = a;
 
-    printf("Enter array elements:\n");
-    for (int i = 0; i < s; i++) {
-        for (int j = 0; j < s; j++) {
-            printf("a[%d][%d] = ", i, j);
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-
-    printf("har of all elements:\n");
-    har(&a[0][0], s);
-
-
+    for (len = 0; *p != '\0' && *p != '\n'; p++, len++  );
+    {
+       	
+    printf("\nThe length of a string is: %d\n", len);
+	}
 
 }
 /*
-Enter array's s: 2
-Enter array elements:
-a[0][0] = 3
-a[0][1] = 2
-a[1][0] = 5
-a[1][1] = 4
-har of all elements:
-27      8       125     64
+Enter any string: hello world
+The length of a string is: 11
 */
